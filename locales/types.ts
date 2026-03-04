5bfa06177850dcd37133930c2b22cfcc58030f3d
Muhammed <muhvmmvdessvm2@gmail.com>
2026-03-04 14:43:33 +0200

    Create types.ts

---
+++ locales/types.ts
@@ -0,0 +1,21 @@
+// locales/types.ts
+
+export type Direction = "rtl" | "ltr";
+
+export interface Translations {
+  dir: Direction;
+  lang: string;
+  font: string;
+
+  nav: any;
+  hero: any;
+  problem: any;
+  framework: any;
+  services: any;
+  comparison: any;
+  social_proof: any;
+  authority: any;
+  cta: any;
+  faq: any;
+  footer: any;
+}
